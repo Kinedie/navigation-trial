a83db5f0fb0c20a9f92a3e16cec27cf5e3963c6a
kinedie <cindy.liwenge@gmail.com>
2020-11-20 16:31:36 +0100

    :tada: init commit, navbar not responsive

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("animatedSidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("animatedSidenav").style.width = "0";
+} 
